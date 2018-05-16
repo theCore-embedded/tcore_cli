d2597ee83b7a24de4214ed53c40408cc8a447f6a
Max Payne <forgge@gmail.com>
2018-05-17 00:28:30 +0300

    fix host compilation

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
   name = 'tcore',
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'theCore C++ embedded framework CLI tools',
   author = 'Max Payne',
   scripts=['tcore'],
   author_email = 'forgge@gmail.com',
   url = 'https://github.com/theCore-embedded/tcore_cli',
-  download_url = 'https://github.com/theCore-embedded/tcore_cli/archive/v0.1.1.tar.gz',
+  download_url = 'https://github.com/theCore-embedded/tcore_cli/archive/v0.1.2.tar.gz',
   keywords = ['embedded', 'cpp', 'c++', 'the_core'],
   classifiers = [],
   install_requires = [ 'tabulate', 'requests', 'coloredlogs' ],
