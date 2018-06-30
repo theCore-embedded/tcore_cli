63fde40d25c9e5a394c99c88b5f2a757808ee1e7
Max Payne <forgge@gmail.com>
2018-06-30 20:09:19 +0300

    bump new version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'tcore',
-  version = '0.2.0',
+  version = '0.2.1',
   description = 'theCore C++ embedded framework CLI tools',
   author = 'Max Payne',
   scripts=['tcore', 'menus.py'],
   packages=find_packages(),
   author_email = 'forgge@gmail.com',
   url = 'https://github.com/theCore-embedded/tcore_cli',
-  download_url = 'https://github.com/theCore-embedded/tcore_cli/archive/v0.2.0.tar.gz',
+  download_url = 'https://github.com/theCore-embedded/tcore_cli/archive/v0.2.1.tar.gz',
   keywords = ['embedded', 'cpp', 'c++', 'the_core'],
   classifiers = [],
   install_requires = [ 'tabulate', 'requests', 'coloredlogs', 'npyscreen', ],
